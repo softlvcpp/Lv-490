2dfe3f6e89bbd3835e90e9d0fec60ccb4bd82a81
Danylo Sadivnychyi <Danylotv43@gmail.com>
2020-04-10 19:18:29 +0300

    Added gmock directory

--- ServerUnitTests/ServerUnitTests.cpp
+++ ServerUnitTests/ServerUnitTests.cpp
@@ -1,4 +1,12 @@
 #include "pch.h"
+#define _HAS_TR1_NAMESPACE 1
+#define _SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING
+#include "gmock/gmock.h"
+#include "../Utility/gtest/gtest/gtest.h"
+#include "../Utility/gtest/gmock-global/gmock-global.h"
+
+//#include "gtest/gtest.h"
+
 #include "CppUnitTest.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
