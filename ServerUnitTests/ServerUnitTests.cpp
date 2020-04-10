8a0c143800e2033f6b10e167534c853368e8e7d2
Danylo Sadivnychyi <Danylotv43@gmail.com>
2020-04-10 19:29:20 +0300

    changed test file

--- ServerUnitTests/ServerUnitTests.cpp
+++ ServerUnitTests/ServerUnitTests.cpp
@@ -2,8 +2,8 @@
 #define _HAS_TR1_NAMESPACE 1
 #define _SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING
 #include "gmock/gmock.h"
-#include "../Utility/gtest/gtest/gtest.h"
-#include "../Utility/gtest/gmock-global/gmock-global.h"
+#include "gtest/gtest.h"
+#include "gmock-global/gmock-global.h"
 
 //#include "gtest/gtest.h"
 
