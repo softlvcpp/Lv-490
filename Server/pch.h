72ed783a546fc62dcb6e0d559ea841b4b3bbd0b2
Oleh Dmytrash <dmutrash61@gmail.com>
2020-04-21 11:05:57 +0300

    Added tests

--- Server/pch.h
+++ Server/pch.h
@@ -3,7 +3,7 @@
 
 #pragma comment(lib, "Ws2_32.lib")
 
-#include <winsock2.h>
+#include <WinSock2.h>
 #include <Windows.h>
 
 #include <memory>
--- ServerUnitTests/pch.h
+++ ServerUnitTests/pch.h
@@ -9,4 +9,38 @@
 
 // add headers that you want to pre-compile here
 
+#define _HAS_TR1_NAMESPACE 1
+#define _SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING
+#define _SILENCE_ALL_CXX17_DEPRECATION_WARNINGS
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "gmock-global/gmock-global.h"
+
+#pragma comment(lib, "Ws2_32.lib")
+
+#include <WinSock2.h>
+#include <Windows.h>
+
+#include <memory>
+#include <string_view>
+#include <iostream>
+#include <fstream>
+#include <mutex>
+#include <condition_variable>
+#include <string>
+#include <vector>
+#include <functional>
+#include <future>
+#include <vector>
+#include <thread>
+#include <queue>
+#include <codecvt>
+#include <ctime>
+
+#include "../Utility/XML_Parser/XMLServer.h"
+#include "../Utility/Logger/LoggerDLL.h"
+#include "../Utility/DatabaseManager/DatabaseManager.h"
+
+
 #endif //PCH_H
